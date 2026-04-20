94886aeff3e020299c253a22a1d6c6c6c5f86782
Amitha Bandara <bandaraamitha317@gmail.com>
2026-04-20 13:41:36 +0530

    first commit

--- src/main/java/com/smartcampus/smart/campus/api/SmartCampusApi.java
+++ src/main/java/com/smartcampus/smart/campus/api/SmartCampusApi.java
@@ -11,6 +11,6 @@ package com.smartcampus.smart.campus.api;
 public class SmartCampusApi {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("");
     }
 }
