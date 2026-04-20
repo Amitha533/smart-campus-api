3e715d57e36c47f14fe09527ccb605587070d788
Amitha Bandara <bandaraamitha317@gmail.com>
2026-04-20 13:09:16 +0530

    first commit

---
+++ src/main/java/com/smartcampus/smart/campus/api/SmartCampusApi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.smartcampus.smart.campus.api;
+
+/**
+ *
+ * @author Amitha Bandara
+ */
+public class SmartCampusApi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
